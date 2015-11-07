1cb0e14bfea0cd91f4dae19f6c79276b0b66c021
aliang <aliang2344@gmail.com>
2015-11-06 23:38:18 -0500

    moving gen_new_id into meta_node

--- src/node.rs
+++ src/node.rs
@@ -1,17 +1,22 @@
-use rand::{thread_rng, Rng};
-
-///FixedSizeArray isn't stable yet to be used as a trait so macros are the only way to achieve easy
-///configurable arrays on the stack (at compile time). The easy way would be to use vectors but
-///those are boxed and therefore heap allocated
-///
-/// our address space is 20 bytes of random fun (160 bits)
+use rand::{thread_rng, Rng, Rand};
+use std::ptr;
 macro_rules! addr_spc { () => { 20 } }
 
 macro_rules! meta_node {
     ($name: ident (id_len = $length: expr)) => {
-        trait $name <T> where T: PartialEq + PartialOrd {
+        trait $name <T> where T: PartialEq + PartialOrd + Rand + Copy{
             fn my_id (&self) -> &[T; $length];
-            fn gen_new_id () -> [T; $length];
+            fn gen_new_id () -> [T; $length] {
+                use std::mem;
+                let mut id_buf:[T; $length] = unsafe {mem::uninitialized()};
+                let mut nrg = thread_rng();
+                let iter = nrg.gen_iter::<T>();
+                //this is kind of a silly way to avoid using the heap
+                for (x, i) in iter.take(addr_spc!()).enumerate() {
+                    id_buf[x] = i;
+                }
+                id_buf
+            }
             fn cmp_dist <'a> (a: &'a[T; $length], b: &'a[T; $length]) -> Option<&'a[T; $length]>{
                 let zipped = a.iter().zip(b.iter());
                 for (_a, _b) in zipped {
@@ -38,16 +43,6 @@ pub struct KademliaNode {
 impl ASizedNode<u8> for KademliaNode {
     fn my_id (&self) -> &[u8; addr_spc!()] {
         &self.addr_id
-    }
-    fn gen_new_id() -> [u8; addr_spc!()] {
-        let mut id_buf = [0; addr_spc!()];
-        let mut nrg = thread_rng();
-        let iter = nrg.gen_iter::<u8>();
-        //this is kind of a silly way to avoid using the heap
-        for (x, i) in iter.take(addr_spc!()).enumerate() {
-            id_buf[x] = i;
-        }
-        id_buf
     }
 }
 
