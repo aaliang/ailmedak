a54a6dcd929e3f7ce5650ce278351c5f1def20b8
aliang <aliang2344@gmail.com>
2015-11-06 22:31:28 -0500

    metaprogramming in rust

--- src/node.rs
+++ src/node.rs
@@ -1,26 +1,41 @@
 use rand::{thread_rng, Rng};
 
-
 ///FixedSizeArray isn't stable yet to be used as a trait so macros are the only way to achieve easy
 ///configurable arrays on the stack (at compile time). The easy way would be to use vectors but
 ///those are boxed and therefore heap allocated
 ///
 /// our address space is 20 bytes of random fun (160 bits)
-macro_rules! addr_spc {
-    () => { 20 };
+macro_rules! addr_spc { () => { 20 } }
+
+macro_rules! meta_node {
+    ($name: ident (id_len = $length: expr)) => {
+        trait $name <T> where T: PartialEq + PartialOrd {
+            fn my_id (&self) -> &[T; $length];
+            fn gen_new_id () -> [T; $length];
+            fn cmp_dist <'a> (a: &'a[T; $length], b: &'a[T; $length]) -> Option<&'a[T; $length]>{
+                let zipped = a.iter().zip(b.iter());
+                for (_a, _b) in zipped {
+                    if _a < _b {
+                        return Some(a)
+                    }
+                    else if _a > _b {
+                        return Some(b)
+                    }
+                }
+                return None
+            }
+        }
+    };
 }
 
-trait BaseNode <T> {
-    fn my_id (&self) -> &T;
-    fn gen_new_id () -> T;
-}
+meta_node!(ASizedNode (id_len = addr_spc!()));
 
 #[derive(Debug)]
 pub struct KademliaNode {
     addr_id: [u8; addr_spc!()]
 }
 
-impl BaseNode <[u8; addr_spc!()]> for KademliaNode {
+impl ASizedNode<u8> for KademliaNode {
     fn my_id (&self) -> &[u8; addr_spc!()] {
         &self.addr_id
     }
@@ -35,7 +50,6 @@ impl BaseNode <[u8; addr_spc!()]> for KademliaNode {
         id_buf
     }
 }
-
 
 impl KademliaNode {
     pub fn new () -> KademliaNode {
