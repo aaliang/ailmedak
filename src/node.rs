e8f2d191922ebbcf0bd836495676813cba2e1874
aliang <aliang2344@gmail.com>
2015-11-07 16:03:54 -0500

    adding dist_as_bytes method

--- src/node.rs
+++ src/node.rs
@@ -1,14 +1,14 @@
 use rand::{thread_rng, Rng, Rand};
-use std::ptr;
+use std::mem;
+
 macro_rules! addr_spc { () => { 20 } }
 
 macro_rules! meta_node {
     ($name: ident (id_len = $length: expr)) => {
-        trait $name <T> where T: PartialEq + PartialOrd + Rand + Copy{
+        trait $name <T> where T: PartialEq + PartialOrd + Rand {
             fn my_id (&self) -> &[T; $length];
             fn gen_new_id () -> [T; $length] {
-                use std::mem;
-                let mut id_buf:[T; $length] = unsafe {mem::uninitialized()};
+                let mut id_buf:[T; $length] = unsafe { mem::uninitialized() };
                 let mut nrg = thread_rng();
                 let iter = nrg.gen_iter::<T>();
                 //this is kind of a silly way to avoid using the heap
@@ -29,20 +29,30 @@ macro_rules! meta_node {
                 }
                 return None
             }
+            fn dist_as_bytes (a: &[T; $length], b: &[T; $length]) -> [T; $length];
         }
     };
 }
 
 meta_node!(ASizedNode (id_len = addr_spc!()));
 
+type NodeAddr = [u8; addr_spc!()];
+
 #[derive(Debug)]
 pub struct KademliaNode {
-    addr_id: [u8; addr_spc!()]
+    addr_id: NodeAddr
 }
 
 impl ASizedNode<u8> for KademliaNode {
-    fn my_id (&self) -> &[u8; addr_spc!()] {
+    fn my_id (&self) -> &NodeAddr {
         &self.addr_id
+    }
+    fn dist_as_bytes(a: &NodeAddr, b: &NodeAddr) -> NodeAddr {
+        let mut dist:NodeAddr = unsafe { mem::uninitialized() };
+        for (i, (_a, _b)) in a.iter().zip(b.iter()).enumerate() {
+            dist[i] = _a ^ _b;
+        }
+        dist
     }
 }
 
